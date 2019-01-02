d89a001a471a7b9362253ca8b1587ba2b86ce237
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-02 15:21:46 +0100

    Added factorial test for factorial button click

---
+++ src/components/Sequencer/Sequencer.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Sequencer from './Sequencer';
+import { shallow, configure } from  'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
+
+describe('<Seqeuncer />', () => {
+
+    it('matches the snapshot', () => {
+        const tree = shallow(<Sequencer />);
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('shows six buttons', () => {
+       const wrapper = shallow(<Sequencer />);
+       const numberOfBtns = wrapper.find('button');
+       expect(numberOfBtns).toHaveLength(6);
+    });
+
+    it('can show factorial sequence when the button is clicked', () => {
+        const wrapper = shallow(<Sequencer />);
+        const factorialButton = wrapper.find('button#factorial');
+        factorialButton.simulate('click', { target: { id: 'factorial' } });
+        wrapper.update();
+        const liItem = wrapper.find('ul.factorial-list>li');
+        expect(liItem).toHaveLength(1);
+    });
+
+
+
+});
