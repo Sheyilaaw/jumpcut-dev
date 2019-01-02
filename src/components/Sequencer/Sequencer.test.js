fff28760e693187990a623c697fe0893ef150502
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-02 22:19:06 +0100

    Added tests for piped sequence generator

--- src/components/Sequencer/Sequencer.test.js
+++ src/components/Sequencer/Sequencer.test.js
@@ -6,11 +6,6 @@ configure({adapter: new Adapter()});
 
 describe('<Seqeuncer />', () => {
 
-    it('matches the snapshot', () => {
-        const tree = shallow(<Sequencer />);
-        expect(tree).toMatchSnapshot();
-    });
-
     it('shows six buttons', () => {
        const wrapper = shallow(<Sequencer />);
        const numberOfBtns = wrapper.find('button');
@@ -56,6 +51,50 @@ describe('<Seqeuncer />', () => {
         });
         wrapper.update();
         const liItem = wrapper.find('ul.range-list>li');
+        expect(liItem).toHaveLength(1);
+    });
+
+    it('can show prime sequence when the button is clicked', () => {
+        const wrapper = shallow(<Sequencer />);
+        const factorialButton = wrapper.find('button#prime');
+
+        factorialButton.simulate('click', {
+            stopPropagation: () => {},
+            target: { id: 'prime' }
+        });
+        wrapper.update();
+        const liItem = wrapper.find('ul.prime-list>li');
+        expect(liItem).toHaveLength(1);
+    });
+
+    it('can show partial sequence when the button is clicked', () => {
+        const wrapper = shallow(<Sequencer />);
+        const factorialButton = wrapper.find('button#partial');
+        //Fill input field for range
+        const partialField = wrapper.find('input#partial-values');
+        partialField.simulate('change', { target: { value: '1 3 7 2 0' } });
+
+        factorialButton.simulate('click', {
+            stopPropagation: () => {},
+            target: { id: 'partial' }
+        });
+        wrapper.update();
+        const liItem = wrapper.find('ul.partial-list>li');
+        expect(liItem).toHaveLength(1);
+    });
+
+    it('can show piped sequence when the button is clicked', () => {
+        const wrapper = shallow(<Sequencer />);
+        const pipedButton = wrapper.find('button#piped');
+        const selectInput = wrapper.find('select#piped-options');
+        selectInput.simulate('change', { target: { value: 'factorial' } });
+
+        pipedButton.simulate('click', {
+            stopPropagation: () => {},
+            target: { id: 'piped' }
+        });
+        wrapper.update();
+        const liItem = wrapper.find('ul.piped-list>li');
         expect(liItem).toHaveLength(1);
     });
 
