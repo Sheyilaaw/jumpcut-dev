dce47d385efe23adde7b5ab87bb6e7ab1d40c3c0
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-02 18:14:28 +0100

    Refactored isPrime function

--- src/components/Generator/Generator.js
+++ src/components/Generator/Generator.js
@@ -55,12 +55,9 @@ const Generator = {
     },
 
     isPrime: function(num) {
-        for ( let i = 2; i < num; i++ ) {
-            if ( num % i === 0 ) {
-                return false;
-            }
-        }
-        return true;
+        for(let i = 2; i < num; i++)
+            if(num % i === 0) return false;
+        return num !== 1 && num !== 0;
     },
 
     primeSeq: function(){
