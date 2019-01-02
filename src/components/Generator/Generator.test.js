c399b5d2035845dfe98684db4514b630af15dc67
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-02 15:22:45 +0100

    Added test to verify factorial sequence generated

--- src/components/Generator/Generator.test.js
+++ src/components/Generator/Generator.test.js
@@ -8,4 +8,12 @@ describe('Generator.js', ()  => {
         expect(factorial).toEqual(120);
     });
 
+    it('generates a factorial sequence', () => {
+        const factorialSequence = Generator.factorialSeq();
+        expect(factorialSequence).toEqual(
+            [ 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880,
+            3628800, 39916800, 479001600, 6227020800, 87178291200 ]
+        );
+    });
+
 });
