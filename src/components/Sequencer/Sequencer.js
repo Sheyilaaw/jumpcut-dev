6322d127f7e075ae0a0f1cbe39e3ad105c4eca1d
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-02 17:16:10 +0100

    Added functionality to display range values generated by range sequencer

--- src/components/Sequencer/Sequencer.js
+++ src/components/Sequencer/Sequencer.js
@@ -6,15 +6,32 @@ class Sequencer extends Component {
     constructor(props){
         super(props);
         this.state = {
+            start: '',
+            step: '',
             sequenceFac: null,
             fibonacciFac:null,
+            rangeFac:null,
             factorial: [],
             fibonacci: [],
+            range: []
         };
 
     }
 
+    handleStartOnChange(e) {
+        this.setState({
+            start: e.target.value
+        });
+    }
+
+    handleStepOnChange(e) {
+        this.setState({
+            step: e.target.value
+        });
+    }
+
     handleClick = (e) => {
+        e.stopPropagation();
         const btnId = e.target.id;
         switch (btnId) {
             case 'factorial':
@@ -45,6 +62,27 @@ class Sequencer extends Component {
                     this.setState({
                         fibonacci: [...this.state.fibonacci, nextSequenceVal]
                     });
+                }
+                break;
+            case 'range':
+                if(this.state.state === '' || this.state.step === ''){
+                    alert('Enter a start and step');
+                }else {
+                    if(this.state.rangeFac == null){
+                        const start = parseInt(this.state.start);
+                        const step = parseInt(this.state.step);
+                        const range = Generator.rangeSeq;
+                        const sequence = Generator.generator(range,start,step);
+                        this.setState({
+                            rangeFac: sequence,
+                            range: [...this.state.range, sequence.next()]
+                        });
+                    } else{
+                        const nextSequenceVal = this.state.rangeFac.next();
+                        this.setState({
+                            range: [...this.state.range, nextSequenceVal]
+                        });
+                    }
                 }
                 break;
             default:
@@ -82,6 +120,18 @@ class Sequencer extends Component {
 
                 <div className="col-md-2" id="range" onClick={this.handleClick}>
                     <button id="range" onClick={ this.handleClick } > Range </button>
+                    <input type="number" id="start" placeholder="Start Number"
+                           value={this.state.start} onChange={ (e) => this.handleStartOnChange(e) } />
+                    <input type="number" id="step" placeholder="Step"
+                           value={this.state.step} onChange={ (e) => this.handleStepOnChange(e) } />
+                    <ul className="range-list">
+                        {
+                            this.state.range.map(function(value) {
+                                return <li key={value}>{value}</li>
+                            })
+                        }
+                    </ul>
+
                 </div>
 
                 <div className="col-md-2" id="prime" onClick={this.handleClick}>
